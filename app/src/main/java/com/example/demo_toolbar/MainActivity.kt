1686d8ffd0d7fc2ab51142e819a466b3e0bcbc6a
Francisco J <fran.lopez@symbyosys.es>
2022-10-11 17:26:24 +0100

    add: toolbar with menu

---
+++ app/src/main/java/com/example/demo_toolbar/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.demo_toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    val tag = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // menuInflater nos permite inyectar un recurso xml (menu_items) en el menu
+        // R. => Carpeta de Recursos de android
+        // menu. => Carpeta del recurso al que queremos acceder
+        menuInflater.inflate(R.menu.menu_items, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_chat -> chat()
+            R.id.action_info -> info()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun info() {
+        Log.d(tag, "Info Clicked")
+    }
+
+    private fun chat() {
+        Log.d(tag, "chat Clicked")
+    }
+}
